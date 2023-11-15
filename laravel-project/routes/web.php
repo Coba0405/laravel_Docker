20a78e50d930c60df6021b21d49355c9c9ae8344
こば <vacky0405@gmail.com>
2023-11-15 23:55:13 +0900

    エラー改善

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    \Illuminate\Support\Facades\Log::info('New log entry created.');
+
     return view('welcome');
 });
 
